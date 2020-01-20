7c4b8f71421110bb5d004cdc10defc9147e0a0f8
akshat <akshatmehrotra2004@gmail.com>
2020-01-20 19:59:48 +0530

    Revert "Update example.jl"
    This reverts commit 24ee05375996fd0f23ecc0b13a629d23ebf0f7f7.

--- exercises/perfect-numbers/example.jl
+++ exercises/perfect-numbers/example.jl
@@ -1,6 +1,3 @@
-## the line bellow has nothing to do with the code
-using Pkg; Pkg.add("LazyJSON")
-
 function classify(number)
     factors = []
     if number == 1
@@ -8,15 +5,15 @@ function classify(number)
     elseif number <= 0
         return "Classification is only possible for natural numbers."
     end
-
+    
     for i in 1:(number-1)
         if number%i == 0
             push!(factors, i)
         end
     end
-    if sum(factors) > number
+    if sum(factors) > number 
         return "abundant"
-    elseif sum(factors) < number
+    elseif sum(factors) < number 
         return "deficient"
     else
         return "perfect"
